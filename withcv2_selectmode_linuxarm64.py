cce573217d11c9e0763f43dbf09360c617cc0548
JetsonNano <pongsagorn.ch@gmail.com>
2021-10-16 03:34:46 +0700

    Improved 15Oct2021 No Succ

---
+++ withcv2_selectmode_linuxarm64.py
@@ -0,0 +1,132 @@
+# Modified from https://github.com/elmonkey/Python_OpenNI2
+# require-package primesense opencv-python-contrib
+import numpy as np
+import cv2
+from primesense import openni2
+from primesense import _openni2 as c_api
+
+openni2.initialize("./Redist_linux/arm64/")     # can also accept the path of the OpenNI redistribution
+
+# Enable / Disable each camera
+'''Note: openni_color(openni_rgb_cam) and IR streams cannot run simultaneously '''
+ENABLE_CAM_COLOR = True
+ENABLE_CAM_DEPTH = True
+ENABLE_CAM_IR = False
+OpenNI_RGB_CAMERA = False # True if rgb_openni_cam ex. Astra ASTRA S or ASTRA MINI S // False if rgb_normal_cam ex. ASTRA PRO or Astra Stereo S U3
+
+# Define Multiply Factor of 16Bits DEPTH / IR
+Depth16_MultiplyFactor = 10
+IR16_MultiplyFactor = 50
+
+dev = openni2.Device.open_any()
+
+if(ENABLE_CAM_COLOR):
+    # ----------------------- RGB Camera INFO
+    if OpenNI_RGB_CAMERA:
+        # ----------------------- Color INFO
+        color_sensor_info = dev.get_sensor_info(sensor_type=openni2.SENSOR_COLOR)
+        color_sensor_mode = color_sensor_info.videoModes
+        print("----------- List of color videoModes -----------")
+        for i,value in enumerate(color_sensor_mode):
+            print(f"{i} -> {value}")
+        color_stream = dev.create_color_stream()
+        print("Enter Color Mode(Number)  <==== ", end="")
+        selectedColorMode = int(input())
+        color_stream.set_video_mode(color_sensor_mode[selectedColorMode])
+        color_mode = color_stream.get_video_mode()
+        print("##### ------> Selected Color Mode(Number) only RBG888 :",color_mode)
+        color_stream.start()
+    else :
+        color_stream = cv2.VideoCapture(0) # if used ni camera please use -> dev.create_color_stream()
+        if not color_stream.isOpened():
+            print("Cannot open color camera!!")
+            exit()
+
+if(ENABLE_CAM_DEPTH):
+    # ----------------------- Depth INFO
+    depth_sensor_info = dev.get_sensor_info(sensor_type=openni2.SENSOR_DEPTH)
+    depth_sensor_mode = depth_sensor_info.videoModes
+    print("----------- List of depth videoModes -----------")
+    for i,value in enumerate(depth_sensor_mode):
+        print(f"{i} -> {value}")
+    depth_stream = dev.create_depth_stream()
+    print("Enter Depth Mode(Number) only16bits  <==== ", end="")
+    selectedDepthMode = int(input())
+    depth_stream.set_video_mode(depth_sensor_mode[selectedDepthMode])
+    depth_mode = depth_stream.get_video_mode()
+    print("##### ------> Selected Depth Mode:",depth_mode)
+    depth_stream.start()
+
+if(ENABLE_CAM_IR):
+    # ----------------------- Infrared INFO
+    ir_sensor_info = dev.get_sensor_info(sensor_type=openni2.SENSOR_IR)
+    ir_sensor_mode = ir_sensor_info.videoModes
+    print("----------- List of IR videoModes -----------")
+    for i,value in enumerate(ir_sensor_mode):
+        print(f"{i} -> {value}")
+    ir_stream = dev.create_ir_stream()
+    print("Enter IR Mode(Number) only16bits <==== ", end="")
+    selectedIRMode = int(input())
+    ir_stream.set_video_mode(ir_sensor_mode[selectedIRMode])
+    ir_mode = ir_stream.get_video_mode()
+    print("##### ------> Selected IR Mode:",ir_mode)
+    ir_stream.start()
+
+
+# define stream fps
+fps = 30
+fetchFrameInterval = int(1000/fps) # 1000/fps
+
+print("started streaming...")
+
+while(cv2.waitKey( fetchFrameInterval )!=27):
+    if(ENABLE_CAM_COLOR):
+        if OpenNI_RGB_CAMERA:
+            color_frame = color_stream.read_frame()
+            color_frame_data = color_frame.get_buffer_as_uint8()
+            color_img = np.fromstring(color_frame_data,dtype=np.uint8).reshape(color_mode.resolutionY,color_mode.resolutionX,3)
+            color_img = cv2.cvtColor(color_img,cv2.COLOR_RGB2BGR)
+        else:
+            ret, color_img = color_stream.read()
+            color_img = cv2.flip(color_img,1)
+
+    if(ENABLE_CAM_DEPTH):
+        depth_frame = depth_stream.read_frame()
+        depth_frame_data = depth_frame.get_buffer_as_uint16()
+        #depth_frame_data = depth_frame.get_buffer_as_uint8()
+
+        depth_img = np.frombuffer(depth_frame_data, dtype=np.uint16)
+        depth_img.shape = (1, depth_mode.resolutionY, depth_mode.resolutionX)
+        depth_img = np.concatenate((depth_img, depth_img, depth_img), axis=0)
+        depth_img = np.swapaxes(depth_img, 0, 2)
+        depth_img = np.swapaxes(depth_img, 0, 1)
+        depth_img *= Depth16_MultiplyFactor
+
+    if(ENABLE_CAM_IR):
+        ir_frame = ir_stream.read_frame()
+        ir_frame_data = ir_frame.get_buffer_as_uint16()
+
+        ir_img = np.frombuffer(ir_frame_data, dtype=np.uint16)
+        ir_img.shape = (1, ir_mode.resolutionY, ir_mode.resolutionX)
+        ir_img = np.concatenate((ir_img, ir_img, ir_img), axis=0)
+        ir_img = np.swapaxes(ir_img, 0, 2)
+        ir_img = np.swapaxes(ir_img, 0, 1)
+        ir_img *= IR16_MultiplyFactor
+
+    if(ENABLE_CAM_COLOR):
+        cv2.imshow("color_img",color_img)
+    if(ENABLE_CAM_DEPTH):
+        cv2.imshow("depth_image", depth_img)
+    if(ENABLE_CAM_IR):
+        cv2.imshow("ir_image", ir_img)
+if(ENABLE_CAM_COLOR):
+    if OpenNI_RGB_CAMERA:
+        color_stream.stop()
+    else:
+        color_stream.release()
+if(ENABLE_CAM_DEPTH):
+    depth_stream.stop()
+if(ENABLE_CAM_IR):
+    ir_stream.stop()
+openni2.unload()
+cv2.destroyAllWindows()
